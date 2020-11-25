2176c124006a103ffd5f9c7a5675f66fd19e1167
lghuntfor <lghuntfor@sina.com>
2020-11-25 12:01:35 +0800

    update file

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-DATA_DIR=/data/appdatas/cat
-LOG_DIR=/data/applogs/cat
-
-#创建数据与日志文件目录
-mkdir -p $DATA_DIR
-mkdir -p $LOG_DIR
-
-#设置目录的权限
-chmod 777 /data/appdatas
-chmod 777 /data/applogs
-
-#拷贝文件
-cp ./client.xml $DATA_DIR
-cp ./datasources.xml $LOG_DIR
-
 #开始构建镜像
 docker build -t cat:3.0.0 .
-
 
--- docker-run.sh
+++ docker-run.sh
@@ -3,6 +3,8 @@
 docker stop cat-server
 docker rm cat-server
 
-docker run -d --name cat-server  -p 2281:2281 -p 2280:2280  --privileged=true --cap-add=SYS_PTRACE -v /data/appdatas:/data/appdatas -v /data/applogs:/data/applogs  cat:3.0.0
+CAT_ADMIN_PWD="12345678"
+
+docker run -d --restart=always --name cat-server -e JAVA_OPTS="-server -Xmx512m -Xms128m" -e CAT_ADMIN_PWD=$CAT_ADMIN_PWD  -p 2281:2281 -p 2280:2280  --privileged=true --cap-add=SYS_PTRACE -v /data/appdatas:/data/appdatas -v /data/applogs:/data/applogs  cat:3.0.0
 
 
